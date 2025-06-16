cb5ace2f5feead62e216573700ea96581cd4d246
Minhchau <minhchau.dang@liferay.com>
2025-06-16 12:01:08 +0900

    LPD-54657 Move Config and related logic to its own file

---
+++ buildSrc/src/main/groovy/com/liferay/docker/workspace/environments/Config.groovy
@@ -0,0 +1,172 @@
+package com.liferay.docker.workspace.environments
+
+import java.util.regex.Pattern
+import java.util.regex.Matcher
+
+import org.gradle.api.Project
+import org.gradle.api.file.ConfigurableFileTree
+import org.gradle.api.file.FileTree
+import org.gradle.api.GradleException
+
+
+class Config {
+
+	public Config(Project project) {
+		this.project = project
+
+		Integer clusterNodes = project.getProperty("lr.docker.environment.cluster.nodes") as Integer
+
+		if (clusterNodes != null) {
+			this.clusterNodes = clusterNodes
+		}
+
+		this.composeFiles.add("docker-compose.yaml")
+
+		this.composeFiles.addAll(this.toList(project.getProperty("lr.docker.environment.compose.files")))
+
+		String databaseName = project.getProperty("lr.docker.environment.database.name")
+
+		if (databaseName != null) {
+			this.databaseName = databaseName
+		}
+
+		String databasePartitioningEnabled = project.getProperty("lr.docker.environment.database.partitioning.enabled")
+
+		if (databasePartitioningEnabled != null) {
+			this.databasePartitioningEnabled = databasePartitioningEnabled.toBoolean()
+		}
+
+		String dataDirectory = project.getProperty("lr.docker.environment.data.directory")
+
+		if (dataDirectory != null && dataDirectory.length() > 0) {
+			this.dataDirectory = dataDirectory
+		}
+
+		List hotfixURLs = this.toList(project.getProperty("lr.docker.environment.hotfix.urls"))
+
+		if (!hotfixURLs.isEmpty()) {
+			this.hotfixURLs = hotfixURLs
+		}
+
+		String namespace = project.getProperty("lr.docker.environment.namespace")
+
+		if (namespace != null) {
+			this.namespace = namespace
+		}
+
+		List services = project.properties.findAll {
+			it.key =~ /^lr.docker.environment.service.enabled\[\w+\]$/
+		}.findAll {
+			it.value =~ /true|1/
+		}.collect {
+			it.key.substring(it.key.indexOf("[") + 1, it.key.indexOf("]"))
+		}
+
+		if (!services.isEmpty()) {
+			this.services = services
+		}
+
+		this.liferayDockerImageId = "${this.namespace.toLowerCase()}-liferay"
+
+		def webserverHostnames = project.getProperty("lr.docker.environment.web.server.hostnames").split(',')*.trim().findAll { it }
+
+		if (webserverHostnames != null) {
+			this.webserverHostnames = webserverHostnames.join(' ')
+		}
+
+		String[] databasePartitioningCompatibleServiceNames = ["mysql", "postgres"]
+		File projectDir = project.projectDir as File
+
+		this.useLiferay = this.services.contains("liferay")
+
+		this.useClustering = this.useLiferay && this.clusterNodes > 0
+
+		this.useWebserver = this.services.contains("webserver")
+
+		ConfigurableFileTree dockerComposeFileTree = project.fileTree(projectDir) {
+			include "**/service.*.yaml"
+
+			if (this.useClustering) {
+				include "**/clustering.*.yaml"
+			}
+
+			if (this.useLiferay) {
+				include "**/liferay.*.yaml"
+			}
+
+			if (this.databasePartitioningEnabled) {
+				if (!this.services.any {databasePartitioningCompatibleServiceNames.contains(it)}) {
+					throw new GradleException("Database partitioning must be used with one of these databases: ${databasePartitioningCompatibleServiceNames}")
+				}
+
+				include "**/database-partitioning.*.yaml"
+			}
+		}
+
+		List<String> serviceComposeFiles = this.services.collect {
+			String serviceName ->
+
+			FileTree matchingFileTree = dockerComposeFileTree.matching {
+				include "**/*.${serviceName}.yaml"
+			}
+
+			if (matchingFileTree.isEmpty()) {
+				List<String> possibleServices = dockerComposeFileTree.findAll{
+					it.name.startsWith("service.")
+				}.collect {
+					it.name.substring("service.".length(), it.name.indexOf(".yaml"))
+				}
+
+				throw new GradleException(
+					"The service '${serviceName}' does not have a matching service.*.yaml file. Possible services are: ${possibleServices}");
+			}
+
+			matchingFileTree.getFiles()
+		}.flatten().collect {
+			projectDir.relativePath(it)
+		}
+
+		this.composeFiles.addAll(serviceComposeFiles)
+
+		this.environmentMap.put "DATA_DIRECTORY", this.dataDirectory
+		this.environmentMap.put "DATABASE_NAME", this.databaseName
+		this.environmentMap.put "NAMESPACE", this.namespace
+
+		if (this.useClustering) {
+			this.environmentMap.put "LIFERAY_CLUSTER_NODES", this.clusterNodes
+		}
+
+		if (this.useLiferay) {
+			this.environmentMap.put "LIFERAY_IMAGE_NAME", this.liferayDockerImageId
+		}
+
+		this.environmentMap.put("COMPOSE_FILE", this.composeFiles.join(File.pathSeparator))
+		this.environmentMap.put("COMPOSE_PROJECT_NAME", this.namespace.toLowerCase())
+	}
+
+	static List toList(String s) {
+		return s.trim().split(",").grep()
+	}
+
+	public Project project
+
+	public int clusterNodes = 0
+	public List<String> composeFiles = new ArrayList<String>()
+	public String databaseName = "lportal"
+	public boolean databasePartitioningEnabled = false
+	public String dataDirectory = "data"
+	public Map<String, String> environmentMap = [:]
+	public List<String> hotfixURLs = new ArrayList<String>()
+	public String liferayDockerImageId = ""
+	public String namespace = "lrswde"
+	public List<String> services = new ArrayList<String>()
+	public boolean useClustering = false
+	public boolean useLiferay = false
+	public boolean useWebserver = false
+	public String webserverHostnames = "localhost"
+
+	@Override
+	public String toString() {
+		return "${this.class.declaredFields.findAll{ !it.synthetic && !it.name.toLowerCase().contains("password") }*.name.collect { "${it}: ${this[it]}" }.join("\n")}"
+	}
+}

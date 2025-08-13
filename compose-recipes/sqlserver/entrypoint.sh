8b743ce538d5e7ff424477d4049ec6a91ae37329
Anthony Chu <anthony.chu@liferay.com>
2025-08-13 10:47:05 -0700

    LPD-62912 Add compose recipe files for SQL Server

---
+++ compose-recipes/sqlserver/entrypoint.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+_sqlcmd="/opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -P ${MSSQL_SA_PASSWORD}"
+
+_is_database_present() {
+    local database_name=${1}
+
+    ${_sqlcmd} -Q "select name from sys.databases" | grep "${database_name}"
+}
+
+create_database() {
+    local database_name=${1}
+
+    if [[ ! $(_is_database_present ${database_name}) ]]; then
+        echo "Could not find database ${database_name}; creating database..."
+
+        sed -i "s,%DATABASE_NAME%,${database_name},g" /init/init.sql
+
+        ${_sqlcmd} -i /init/init.sql
+
+        return
+    else
+        echo "Database ${database_name} is present; skipping database creation"
+
+        return
+    fi
+}
+
+main() {
+    until ${_sqlcmd} -Q "SELECT 1"; do
+        sleep 1
+        echo "[entrypoint] Waiting for SQL Server to be available..."
+    done
+
+    create_database ${COMPOSER_DATABASE_NAME}
+}
+
+main & /opt/mssql/bin/sqlservr
+
+wait

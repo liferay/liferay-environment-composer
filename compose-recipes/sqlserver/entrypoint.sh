70e201341888bbf274e4484223f71ae82fd6e7e7
Anthony Chu <anthony.chu@liferay.com>
2025-08-18 12:59:30 -0700

    LPD-62912 Don't create database if file already exists or database already exists

--- compose-recipes/sqlserver/entrypoint.sh
+++ compose-recipes/sqlserver/entrypoint.sh
@@ -5,7 +5,11 @@ _sqlcmd="/opt/mssql-tools18/bin/sqlcmd -C -S localhost -U sa -P ${MSSQL_SA_PASSW
 _is_database_present() {
 	local database_name=${1}
 
-	${_sqlcmd} -Q "select name from sys.databases" | grep "${database_name}"
+	if	[[ $(ls /opt/mssql/data/ | grep ${database_name}) ]] || \
+		[[ $(${_sqlcmd} -Q "select name from sys.databases" | grep "${database_name}") ]]; then
+
+		echo true
+	fi
 }
 
 create_database() {

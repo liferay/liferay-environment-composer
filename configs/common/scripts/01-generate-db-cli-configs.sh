8e1e049930c52890952124ed65fd96c84df6889b
Drew Brokke <drew.brokke@liferay.com>
2025-08-26 12:55:41 -0500

    LPS-00000 WIP

---
+++ configs/common/scripts/01-generate-db-cli-configs.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+# set -o errexit
+
+# source /mnt/liferay/scripts/_common.sh
+
+DATABASE_HOST="database--route"
+
+  # 180       write "            - LCP_LIFERAY_UPGRADE_ENABLED=\${LCP_LIFERAY_UPGRADE_ENABLED:-}"
+  # 181:      write "            - LCP_SECRET_DATABASE_HOST=database"
+  # 182       write "            - LCP_SECRET_DATABASE_PASSWORD=password"
+  # 183       write "            - LCP_SECRET_DATABASE_USER=root"
+
+
+main() {
+    DATABASE_HOST="database"
+    DATABASE__ROUTE_SERVICE_PORT="3306"
+    LCP_SECRET_DATABASE_NAME="lportal"
+    LCP_SECRET_DATABASE_USER="root"
+
+    generate_config_mysql_client
+    # generate_config_postgres_client
+}
+
+generate_config_mysql_client() {
+  echo -e "\n[SaaS] Mysql Client Configuration"
+  echo -e "\n[SaaS] Generating my.conf file for mysql-client"
+
+  echo -e "\thost=${DATABASE_HOST}"
+  echo -e "\tport=${DATABASE__ROUTE_SERVICE_PORT}"
+  echo -e "\tdatabase=${LCP_SECRET_DATABASE_NAME}"
+  echo -e "\tuser=${LCP_SECRET_DATABASE_USER}"
+
+  CONFIG_FILE=~/.my.cnf
+
+  echo -e "[mysql] \nhost=\"${DATABASE_HOST}\" \nport=\"${DATABASE__ROUTE_SERVICE_PORT}\" \ndatabase=\"${LCP_SECRET_DATABASE_NAME}\" \nuser=\"${LCP_SECRET_DATABASE_USER}\" \npassword=\"${LCP_SECRET_DATABASE_PASSWORD}\"" > ${CONFIG_FILE}
+
+  chmod 600 ${CONFIG_FILE}
+}
+
+generate_config_postgres_client() {
+  echo -e "\n[SaaS] PostgresSQL Client Configuration"
+  echo -e "\n[SaaS] Generating .pgpass file for PostgresSQL psql client"
+
+  export PGUSER=${LCP_SECRET_DATABASE_USER}
+
+  CONFIG_FILE=~/.pgpass
+
+  echo -e "${DATABASE_HOST}:${DATABASE__ROUTE_SERVICE_PORT}:${LCP_SECRET_DATABASE_NAME}:${LCP_SECRET_DATABASE_USER}:${LCP_SECRET_DATABASE_PASSWORD}" > ${CONFIG_FILE}
+
+  chmod 600 ${CONFIG_FILE}
+
+  add_to_bash_rc "export PGDATABASE=${LCP_SECRET_DATABASE_NAME}"
+  add_to_bash_rc "export PGHOST=${DATABASE_HOST}"
+  add_to_bash_rc "export PGUSER=${LCP_SECRET_DATABASE_USER}"
+
+  source ~/.bashrc
+}
+
+add_to_bash_rc() {
+    echo -e "\n${1}" >> ~/.bashrc
+}
+
+main "$@"
---
+++ configs/common/scripts/dofoo.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo ""
+echo ""
+echo ""
+# echo "Sleeping before..."
+# sleep 10s
+echo "ASDF ASDF ASDF"
+echo "ASDF ASDF ASDF"
+which mysql
+# mysql -u root "show databases;" || echo "DB was not valid"
+echo "IN THE MIDDLE"
+# which psql
+echo "ASDF ASDF ASDF"
+echo "ASDF ASDF ASDF"
+echo ""
+# echo "Sleeping after..."
+# sleep 10s
+echo ""
+echo ""

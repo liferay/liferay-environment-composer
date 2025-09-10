0da5db36dc87a4c7fff6552fa99ab695d58a13e0
Anthony Chu <anthony.chu@liferay.com>
2025-09-09 17:00:47 -0700

    LPD-58281 Add script for recreating document library file references

---
+++ scripts/import_doc_lib.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+SOURCE_DIR=${1}
+DEST_DIR=${2}
+
+mkdir -p "${DEST_DIR}"
+
+FILEPATHS=($(find "${SOURCE_DIR}" -type f))
+
+for FILEPATH in ${FILEPATHS[@]}; do
+    if [[ ${SOURCE_DIR} == */ ]]; then
+        SOURCE_DIR="${SOURCE_DIR::-1}"
+    fi
+
+    _FILEPATH=$(dirname ${FILEPATH} | sed "s,${SOURCE_DIR},,")
+
+    if [[ ${_FILEPATH} ]]; then
+        mkdir -p "${DEST_DIR}/${_FILEPATH}"
+    fi
+
+    touch "${DEST_DIR}/${_FILEPATH}/$(basename ${FILEPATH})"
+done

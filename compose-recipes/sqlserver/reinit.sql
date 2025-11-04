a76dd74d2ff53c2312485d6484e0a2f060f330b7
Anthony Chu <anthony.chu@liferay.com>
2025-11-03 16:21:22 -0800

    LPD-70485 Do so for restore also

--- compose-recipes/sqlserver/reinit.sql
+++ compose-recipes/sqlserver/reinit.sql
@@ -1,6 +1,7 @@
 USE master
 GO
 CREATE DATABASE %DATABASE_NAME%
+ALTER DATABASE %DATABASE_NAME% set read_committed_snapshot on
 ON (FILENAME = /var/opt/mssql/data/%DATABASE_NAME%.mdf),
 (FILENAME = /var/opt/mssql/data/%DATABASE_NAME%.ldf)
 FOR ATTACH

5986df42510e5c20a7381e165bc8ca478fa76e0a
Anthony Chu <anthony.chu@liferay.com>
2026-01-28 15:37:17 -0800

    LPD-77148 SF

--- compose-recipes/db2/conf/import-database.sh
+++ compose-recipes/db2/conf/import-database.sh
@@ -3,9 +3,9 @@ backup_dir="/database/data/${DB2INSTANCE}/backups"
 gz_archive=$(find "${backup_dir}" -type f -iname "*.tgz")
 
 if [[ "${gz_archive}" != "" ]] && [[ -f "${gz_archive}" ]]; then
-    tar -pxf "${gz_archive}" -C "${backup_dir}"
+	tar -pxf "${gz_archive}" -C "${backup_dir}"
 
-    rm -rf "${gz_archive}"
+	rm -rf "${gz_archive}"
 fi
 
 echo ${DB2INST1_PASSWORD} | su ${DB2INSTANCE} <<EOSU
@@ -13,13 +13,13 @@ echo ${DB2INST1_PASSWORD} | su ${DB2INSTANCE} <<EOSU
 . /database/config/${DB2INSTANCE}/sqllib/db2profile
 
 if [[ $(find /database/data/${DB2INSTANCE}/backups -iname 'db2move.lst') ]]; then
-    cd /database/data/${DB2INSTANCE}/backups
+	cd /database/data/${DB2INSTANCE}/backups
 
-    db2move ${COMPOSER_DATABASE_NAME} import
+	db2move ${COMPOSER_DATABASE_NAME} import
 
-    rm -rf /database/data/${DB2INSTANCE}/backups/*
+	rm -rf /database/data/${DB2INSTANCE}/backups/*
 else
-    echo "Skipping database import as no dump was found"
+	echo "Skipping database import as no dump was found"
 fi
 
 EOSU

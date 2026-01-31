965a5cc8d68f3e58efad1dcfa38552980ca12b05
Anthony Chu <anthony.chu@liferay.com>
2026-01-30 16:06:39 -0800

    LPD-77148 Make script more readable

--- compose-recipes/db2/conf/restore-database.sh
+++ compose-recipes/db2/conf/restore-database.sh
@@ -1,18 +1,34 @@
 echo "${DB2INST1_PASSWORD}" | su ${DB2INSTANCE} <<EOSU
 . /database/config/${DB2INSTANCE}/sqllib/db2profile
-[[ -f "$(find /database/data/${DB2INSTANCE}/backups -type f | tail -n 1)" ]] && \
-db2 connect to ${COMPOSER_DATABASE_NAME} && \
-db2 force application all && \
-db2 terminate && \
-db2stop force && \
-ipclean -a && \
-db2set -null DB2COMM && \
-db2start admin mode restricted access && \
-db2 RESTORE DATABASE ${COMPOSER_DATABASE_NAME} FROM /database/data/${DB2INSTANCE}/backups INTO ${COMPOSER_DATABASE_NAME} REPLACE EXISTING WITHOUT ROLLING FORWARD && \
-db2stop force && \
-ipclean -a && \
-db2set DB2COMM=TCPIP && \
-db2start && \
-db2 activate db ${COMPOSER_DATABASE_NAME} || \
-echo "Skipping database restore as no dump was found"
+
+if [[ -f "$(find /database/data/${DB2INSTANCE}/backups -type f | tail -n 1)" ]]; then
+	db2 connect to ${COMPOSER_DATABASE_NAME}
+
+	db2 force application all
+
+	db2 terminate
+
+	db2stop force
+
+	ipclean -a
+
+	db2set -null DB2COMM
+
+	db2start admin mode restricted access
+
+	db2 RESTORE DATABASE ${COMPOSER_DATABASE_NAME} FROM /database/data/${DB2INSTANCE}/backups INTO ${COMPOSER_DATABASE_NAME} REPLACE EXISTING WITHOUT ROLLING FORWARD
+
+	db2stop force
+
+	ipclean -a
+
+	db2set DB2COMM=TCPIP
+
+	db2start
+
+	db2 activate db ${COMPOSER_DATABASE_NAME}
+else
+	echo "Skipping database restore as no dump was found"
+fi
+
 EOSU

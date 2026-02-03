5e0f4ec69a68df1023219ce63bb7ca1266222c01
Anthony Chu <anthony.chu@liferay.com>
2026-02-03 11:33:29 -0800

    LPD-77148 Pass function declaration and execution into su

--- compose-recipes/db2/conf/prepare-database.sh
+++ compose-recipes/db2/conf/prepare-database.sh
@@ -1,49 +1,50 @@
-echo ${DB2INST1_PASSWORD} | su ${DB2INSTANCE} <<EOSU
-. /database/config/${DB2INSTANCE}/sqllib/db2profile
+_prepare_database() {
+	. /database/config/${DB2INSTANCE}/sqllib/db2profile
 
-if [[ ! -d /database/data/${DB2INSTANCE}/NODE0000/${COMPOSER_DATABASE_NAME^^} ]]; then
-	echo "[prepare-database.sh] Could not find database ${COMPOSER_DATABASE_NAME^^}. Creating database..."
+	if [[ ! -d /database/data/${DB2INSTANCE}/NODE0000/${COMPOSER_DATABASE_NAME^^} ]]; then
+		echo "[prepare-database.sh] Could not find database ${COMPOSER_DATABASE_NAME^^}. Creating database..."
 
-	db2 create db ${COMPOSER_DATABASE_NAME} pagesize 32768 temporary tablespace managed by automatic storage
-fi
+		db2 create db ${COMPOSER_DATABASE_NAME} pagesize 32768 temporary tablespace managed by automatic storage
+	fi
 
-if [[ -f "$(find /database/data/${DB2INSTANCE}/backups -iname 'db2move.lst')" ]]; then
-	echo "[prepare-database.sh] Found db2move.lst file. Importing database..."
+	if [[ -f "$(find /database/data/${DB2INSTANCE}/backups -iname 'db2move.lst')" ]]; then
+		echo "[prepare-database.sh] Found db2move.lst file. Importing database..."
 
-	cd /database/data/${DB2INSTANCE}/backups
+		cd /database/data/${DB2INSTANCE}/backups
 
-	db2move ${COMPOSER_DATABASE_NAME} import
+		db2move ${COMPOSER_DATABASE_NAME} import
 
-elif [[ -f "$(find /database/data/${DB2INSTANCE}/backups -type f | tail -n 1)" ]]; then
-	echo "[prepare-database.sh] Found dump file. Restoring database..."
+	elif [[ -f "$(find /database/data/${DB2INSTANCE}/backups -type f | tail -n 1)" ]]; then
+		echo "[prepare-database.sh] Found dump file. Restoring database..."
 
-	db2 connect to ${COMPOSER_DATABASE_NAME}
+		db2 connect to ${COMPOSER_DATABASE_NAME}
 
-	db2 force application all
+		db2 force application all
 
-	db2 terminate
+		db2 terminate
 
-	db2stop force
+		db2stop force
 
-	ipclean -a
+		ipclean -a
 
-	db2set -null DB2COMM
+		db2set -null DB2COMM
 
-	db2start admin mode restricted access
+		db2start admin mode restricted access
 
-	db2 RESTORE DATABASE ${COMPOSER_DATABASE_NAME} FROM /database/data/${DB2INSTANCE}/backups INTO ${COMPOSER_DATABASE_NAME} REPLACE EXISTING WITHOUT ROLLING FORWARD
+		db2 RESTORE DATABASE ${COMPOSER_DATABASE_NAME} FROM /database/data/${DB2INSTANCE}/backups INTO ${COMPOSER_DATABASE_NAME} REPLACE EXISTING WITHOUT ROLLING FORWARD
 
-	db2stop force
+		db2stop force
 
-	ipclean -a
+		ipclean -a
 
-	db2set DB2COMM=TCPIP
+		db2set DB2COMM=TCPIP
 
-	db2start
+		db2start
 
-	db2 activate db ${COMPOSER_DATABASE_NAME}
-fi
+		db2 activate db ${COMPOSER_DATABASE_NAME}
+	fi
+}
 
-EOSU
+echo ${DB2INST1_PASSWORD} | su ${DB2INSTANCE} -c "$(declare -f _prepare_database); _prepare_database"
 
 echo "STARTING_UP" > /startup_log.txt

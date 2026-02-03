19155cc6c2c1fd846c2b7a9d579c4bc83110271c
Anthony Chu <anthony.chu@liferay.com>
2026-02-03 11:28:16 -0800

    LPD-77148 Log when actions are taken rather than when not

--- compose-recipes/db2/conf/prepare-database.sh
+++ compose-recipes/db2/conf/prepare-database.sh
@@ -2,22 +2,24 @@ echo ${DB2INST1_PASSWORD} | su ${DB2INSTANCE} <<EOSU
 . /database/config/${DB2INSTANCE}/sqllib/db2profile
 
 if [[ ! -d /database/data/${DB2INSTANCE}/NODE0000/${COMPOSER_DATABASE_NAME^^} ]]; then
+	echo "[prepare-database.sh] Could not find database ${COMPOSER_DATABASE_NAME^^}. Creating database..."
+
 	db2 create db ${COMPOSER_DATABASE_NAME} pagesize 32768 temporary tablespace managed by automatic storage
-else
-	echo "Skipping database creation as database ${COMPOSER_DATABASE_NAME^^} is already present"
 fi
 
 if [[ -f "$(find /database/data/${DB2INSTANCE}/backups -iname 'db2move.lst')" ]]; then
+	echo "[prepare-database.sh] Found db2move.lst file. Importing database..."
+
 	cd /database/data/${DB2INSTANCE}/backups
 
 	db2move ${COMPOSER_DATABASE_NAME} import
 
 	rm -rf /database/data/${DB2INSTANCE}/backups/*
-else
-	echo "Skipping database import as no dump was found"
 fi
 
 if [[ -f "$(find /database/data/${DB2INSTANCE}/backups -type f | tail -n 1)" ]]; then
+	echo "[prepare-database.sh] Found dump file. Restoring database..."
+
 	db2 connect to ${COMPOSER_DATABASE_NAME}
 
 	db2 force application all
@@ -43,8 +45,6 @@ if [[ -f "$(find /database/data/${DB2INSTANCE}/backups -type f | tail -n 1)" ]];
 	db2start
 
 	db2 activate db ${COMPOSER_DATABASE_NAME}
-else
-	echo "Skipping database restore as no dump was found"
 fi
 
 EOSU

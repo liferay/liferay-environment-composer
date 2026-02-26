4a8f419df5ca70b70406b6e47dd5b9f85266ad18
Anthony Chu <anthony.chu@liferay.com>
2026-02-26 15:56:35 -0800

    LPD-77149 Add helper function for downloading database dumps

--- test-database-import.sh
+++ test-database-import.sh
@@ -10,6 +10,18 @@ _clean_dumps_dir() {
 	fi
 }
 
+_download_database_dumps() {
+	local urls=($(curl -s https://storage.googleapis.com/storage/v1/b/liferay-devtools/o?prefix=liferay-environment-composer/test-resources/database-support-tests | jq -r '.items[] | select(.name | contains(".")) | .mediaLink'))
+
+	local baseUrl="https://storage.googleapis.com/download/storage/v1/b/liferay-devtools/o/liferay-environment-composer%2Ftest-resources%2Fdatabase-support-tests%2F"
+
+	for url in ${urls[@]}; do
+		local filepathUrl=$(echo ${url//${baseUrl}/} | sed -e 's@%2F@/@g' -e 's@?.*@@g')
+
+		curl -s -o testDependencies/${filepathUrl//${baseUrl}/} ${url} --create-dirs
+	done
+}
+
 _stop_composer() {
 	local databaseType=${1}
 

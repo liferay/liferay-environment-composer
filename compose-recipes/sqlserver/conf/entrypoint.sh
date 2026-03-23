ab39a4ccff934e8cb458332218ff98e1c15386d0
Anthony Chu <anthony.chu@liferay.com>
2026-03-23 13:42:00 -0700

    LPD-83583 Shellcheck

--- compose-recipes/sqlserver/conf/entrypoint.sh
+++ compose-recipes/sqlserver/conf/entrypoint.sh
@@ -13,7 +13,7 @@ _has_database_files() {
 _is_database_present() {
 	local database_name=${1}
 
-	if	[[ $(${_sqlcmd} -Q "select name from sys.databases" | grep "${database_name}") ]]; then
+	if ${_sqlcmd} -Q "select name from sys.databases" | grep -q "${database_name}"; then
 
 		echo true
 	fi
@@ -22,7 +22,7 @@ _is_database_present() {
 create_database() {
 	local database_name=${1}
 
-	if [[ $(_is_database_present ${database_name}) ]]; then
+	if [[ $(_is_database_present "${database_name}") ]]; then
 		echo "[entrypoint] Database ${database_name} is present; skipping database creation"
 
 		touch /tmp/database_exists
@@ -30,7 +30,9 @@ create_database() {
 		return
 	fi
 
-	local backup_file=$(find /var/opt/mssql/backups -iname "*.bak" -or -iname "*.bacpac" | tail -n 1)
+	local backup_file
+
+	backup_file=$(find /var/opt/mssql/backups -iname "*.bak" -or -iname "*.bacpac" | tail -n 1)
 
 	if [[ -f "${backup_file}" ]]; then
 		echo "[entrypoint] Database backup found; restoring database ${database_name}..."
@@ -58,7 +60,7 @@ create_database() {
 		fi
 	fi
 
-	if [[ $(_has_database_files ${database_name}) ]]; then
+	if [[ $(_has_database_files "${database_name}") ]]; then
 		echo "Database files found; reattaching database ${database_name}..."
 
 		sed -i "s,%DATABASE_NAME%,${database_name},g" /init/reinit.sql
@@ -84,19 +86,19 @@ PID=$!
 
 echo "[entrypoint] Waiting for SQL Server to boot..."
 until [[ -f /var/opt/mssql/log/errorlog ]]; do
-sleep 1
+	sleep 1
 done
 
 until grep -q "SQL Server is now ready for client connections" /var/opt/mssql/log/errorlog; do
-sleep 1
+	sleep 1
 done
 
 until [[ $(${_sqlcmd} -Q "select 1") ]]; do
-sleep 1
+	sleep 1
 done
 
 echo "[entrypoint] SQLServer is available"
 
-create_database ${COMPOSER_DATABASE_NAME}
+create_database "${COMPOSER_DATABASE_NAME}"
 
 wait $PID

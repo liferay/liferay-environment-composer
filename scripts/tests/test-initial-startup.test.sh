bf3ec78795b2679658253c705fb19ee5b1eb6161
Drew Brokke <drew.brokke@liferay.com>
2026-03-30 16:54:09 -0500

    LPD-83621 test: uses helpers now that they are available

--- scripts/tests/test-initial-startup.test.sh
+++ scripts/tests/test-initial-startup.test.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 
-_debug() {
-	if [[ "${DEBUG}" -gt 0 ]]; then
-		echo "    # ${*}" >&3
-	fi
-}
-_lec() {
-	"${WORKSPACE_DIR}/scripts/cli/lec.sh" "${@}"
-}
-_timestamp() {
-	date +%s
-}
+load helpers/setup
+
 _test_initial_startup() {
 	local databaseType=${1}
 
@@ -30,40 +21,18 @@ _test_initial_startup() {
 }
 
 setup_file() {
-	_debug "TEARDOWN FILE ${BATS_TEST_NAME}"
-
-	WORKSPACE_DIR="$(git rev-parse --show-toplevel)"
-	export WORKSPACE_DIR
-
 	BATS_TEST_NAME_PREFIX="Test initial startup "
 	export BATS_TEST_NAME_PREFIX
 
-	LIFERAY_ENVIRONMENT_COMPOSER_HOME="${WORKSPACE_DIR}"
-	export LIFERAY_ENVIRONMENT_COMPOSER_HOME
-
-	LIFERAY_ENVIRONMENT_COMPOSER_WORKSPACES_DIR="${BATS_SUITE_TMPDIR}"
-	export LIFERAY_ENVIRONMENT_COMPOSER_WORKSPACES_DIR
+	common_setup_file
 }
+
 setup() {
-	_debug "SETUP ${BATS_TEST_NAME}"
-
-	local name
-	name="test-initial-startup-$(_timestamp)"
-
-	_lec init "${name}" dxp-2025.q4.12
-
-	TEST_WORKSPACE_DIR="${LIFERAY_ENVIRONMENT_COMPOSER_WORKSPACES_DIR}/lec-${name}"
-	export TEST_WORKSPACE_DIR
-
-	cd "${TEST_WORKSPACE_DIR}" || exit 1
+	common_setup
 }
+
 teardown() {
-	_debug "TEARDOWN ${BATS_TEST_NAME}"
-
-	docker compose down -v
-
-	_lec fn _clean "${TEST_WORKSPACE_DIR}"
-	_lec fn _removeWorktree "${TEST_WORKSPACE_DIR}"
+	common_teardown
 }
 
 @test "db2 initial startup" {

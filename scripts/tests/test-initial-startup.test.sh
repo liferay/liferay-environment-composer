d43b56152d4753f67721a9d033e903679c6336b8
Anthony Chu <anthony.chu@liferay.com>
2026-03-31 11:25:00 -0700

    LPD-83621 Add check for database and liferay service after startup

--- scripts/tests/test-initial-startup.test.sh
+++ scripts/tests/test-initial-startup.test.sh
@@ -19,6 +19,26 @@ _test_initial_startup() {
 
 		return 1
 	fi
+
+	local sqlQueryOutput
+
+	sqlQueryOutput=$(./gradlew executeSQLQuery -PsqlQuery="select emailAddress from User_ where screenName = 'test';")
+
+	if [[ ! ${sqlQueryOutput} =~ test@liferay.com ]]; then
+		echo "[FAILED] expected data not found"
+
+		return 2
+	fi
+
+	local http_response_code
+
+	http_response_code="$(curl -s -o /dev/null -w' %{http_code}' http://localhost:8080)"
+
+	if [[ ${http_response_code} != "200" ]]; then
+		echo "[FAILED] expected response code not found"
+
+		return 3
+	fi
 }
 
 setup_file() {

72a0c6094a6020a576bf371f074021cde43163b4
Anthony Chu <anthony.chu@liferay.com>
2026-04-17 14:35:23 -0700

    LPD-86437 SF

--- compose-recipes/db2/conf/prepare-database.sh
+++ compose-recipes/db2/conf/prepare-database.sh
@@ -1,16 +1,20 @@
+#!/bin/bash
+
 _prepare_database() {
-	. /database/config/${DB2INSTANCE}/sqllib/db2profile
+	# shellcheck disable=SC1090
+	. "/database/config/${DB2INSTANCE}/sqllib/db2profile"
 
 	if [[ ! -d /database/data/${DB2INSTANCE}/NODE0000/${COMPOSER_DATABASE_NAME^^} ]]; then
 		echo "[prepare-database.sh] Could not find database ${COMPOSER_DATABASE_NAME^^}. Creating database..."
 
-		db2 create db ${COMPOSER_DATABASE_NAME} pagesize 32768 temporary tablespace managed by automatic storage
+		db2 create db "${COMPOSER_DATABASE_NAME}" pagesize 32768 temporary tablespace managed by automatic storage
 	fi
 
-	if [[ -f "$(find /database/data/${DB2INSTANCE}/backups -iname 'db2move.lst')" ]]; then
+	if [[ -f "$(find "/database/data/${DB2INSTANCE}/backups" -iname 'db2move.lst')" ]]; then
 		echo "[prepare-database.sh] Found db2move.lst file. Importing database..."
 
-		cd /database/data/${DB2INSTANCE}/backups
+		cd "/database/data/${DB2INSTANCE}/backups" || exit
+
 		local old_schema
 
 		old_schema=$(cat db2move.lst | head -1 | sed "s@\!\"\(.*\)\"\..*@\1@g"; printf x)
@@ -19,12 +23,12 @@ _prepare_database() {
 
 		sed -i "s@\(\!\"\).*\(\"\.\)@\1${DB2INSTANCE^^}\2@g" db2move.lst
 
-		db2move ${COMPOSER_DATABASE_NAME} import
+		db2move "${COMPOSER_DATABASE_NAME}" import
 
-	elif [[ -f "$(find /database/data/${DB2INSTANCE}/backups -type f | tail -n 1)" ]]; then
+	elif [[ -f "$(find "/database/data/${DB2INSTANCE}/backups" -type f | tail -n 1)" ]]; then
 		echo "[prepare-database.sh] Found dump file. Restoring database..."
 
-		db2 connect to ${COMPOSER_DATABASE_NAME}
+		db2 connect to "${COMPOSER_DATABASE_NAME}"
 
 		db2 force application all
 
@@ -38,7 +42,7 @@ _prepare_database() {
 
 		db2start admin mode restricted access
 
-		db2 RESTORE DATABASE ${COMPOSER_DATABASE_NAME} FROM /database/data/${DB2INSTANCE}/backups INTO ${COMPOSER_DATABASE_NAME} REPLACE EXISTING WITHOUT ROLLING FORWARD
+		db2 RESTORE DATABASE "${COMPOSER_DATABASE_NAME}" FROM "/database/data/${DB2INSTANCE}/backups" INTO "${COMPOSER_DATABASE_NAME}" REPLACE EXISTING WITHOUT ROLLING FORWARD WITHOUT PROMPTING
 
 		db2stop force
 
@@ -48,10 +52,10 @@ _prepare_database() {
 
 		db2start
 
-		db2 activate db ${COMPOSER_DATABASE_NAME}
+		db2 activate db "${COMPOSER_DATABASE_NAME}"
 	fi
 }
 
-echo ${DB2INST1_PASSWORD} | su ${DB2INSTANCE} -c "$(declare -f _prepare_database); _prepare_database"
+echo "${DB2INST1_PASSWORD}" | su "${DB2INSTANCE}" -c "$(declare -f _prepare_database); _prepare_database"
 
 echo "STARTING_UP" > /startup_log.txt

6e96eb5599e2b4572f91b410742da3c3f181d8ca
Anthony Chu <anthony.chu@liferay.com>
2026-05-19 09:02:12 -0700

    LPD-89887 Always enable Elasticsearch production mode

--- buildSrc/src/main/groovy/com/liferay/docker/workspace/environments/Config.groovy
+++ buildSrc/src/main/groovy/com/liferay/docker/workspace/environments/Config.groovy
@@ -338,7 +338,6 @@ class Config {
 			}
 
 			this.elasticsearchMajorVersion = elasticsearchMajorVersion
-			this.elasticsearchProductionModeEnabled = elasticsearchMajorVersion == "7" ? "false" : "true"
 		}
 
 		if (this.dockerImageLiferay.contains("7.4") || this.dockerImageLiferay.contains(".q")) {
@@ -470,7 +469,6 @@ class Config {
 	public String dockerImageLiferay = null
 	public boolean dockerImageLiferayDXP = false
 	public String elasticsearchMajorVersion = ""
-	public String elasticsearchProductionModeEnabled = ""
 	public List<String> gcpHotfixURLs = new ArrayList<String>()
 	public boolean glowrootEnabled = false
 	public List<String> hotfixURLs = new ArrayList<String>()
